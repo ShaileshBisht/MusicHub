a72919d3c60a0f0801efb4c93441fdf387bfacea
shaileh Bisht <shaileshbisht765@gmail.com>
2022-10-04 00:02:03 +0530

    completed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -19,7 +19,41 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({
       query: () => createRequest("/charts/world"),
     }),
+    getSongsByGenre: builder.query({
+      query: (genre) =>
+        createRequest(`/charts/genre-world?genre_code=${genre}`),
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) =>
+        createRequest(`/tracks/details?track_id=${songid}`),
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) =>
+        createRequest(`/tracks/related?track_id=${songid}`),
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) =>
+        createRequest(`/artists/details?artist_id=${artistId}`),
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) =>
+        createRequest(`/charts/country?country_code=${countryCode}`),
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        createRequest(
+          `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`
+        ),
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery,
+} = shazamCoreApi;
